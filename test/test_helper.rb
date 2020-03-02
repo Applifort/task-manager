da67cd9311f9304303243c6e04e283c2ed241238
GabitovArtur <coolermast@gmail.com>
2020-03-02 14:49:06 +0500

    authorization

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -14,13 +14,23 @@ require_relative '../config/environment'
 require 'rails/test_help'
 
 class ActiveSupport::TestCase
-  # Run tests in parallel with specified workers
   include FactoryBot::Syntax::Methods
   include AuthHelper
-  # parallelize(workers: :number_of_processors)
-
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
-  # fixtures :all
-
-  # Add more helper methods to be used by all tests here...
 end
+
+module SignInHelper
+  def sign_in_as(admin)
+    post session_path, params: {
+      session: {
+        password: admin.password,
+        email: admin.email
+      }
+    }
+  end
+end
+
+class ActionDispatch::IntegrationTest
+  include SignInHelper
+end
+
+
