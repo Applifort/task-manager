c79b1e0bd5db4cf4381cfdeb8022889f2dbdaa32
GabitovArtur <coolermast@gmail.com>
2020-03-04 16:36:09 +0500

    add kanban, react,webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,10 +2,14 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
+import WebpackerReact from 'webpacker-react';
+import TaskBoard from './components/TaskBoard';
 
-require('@rails/ujs').start();
-require('@rails/activestorage').start();
-require('../channels');
+// require('@rails/ujs').start();
+// require('@rails/activestorage').start();
+// require('../channels');
+
+WebpackerReact.setup({ TaskBoard });
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
@@ -13,3 +17,5 @@ require('../channels');
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+// Support component names relative to this directory:
+
